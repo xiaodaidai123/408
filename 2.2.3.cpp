b00ba55bb07f87e568191421feabed4aeea8f366
zyz-macbook <1312297046@qq.com>
2026-05-09 11:33:42 +0800

    2026-0509 update

--- 2.2.3.cpp
+++ 2.2.3.cpp
@@ -84,12 +84,22 @@ void T5(SqList &L)
     //从有序顺序表中删除所有其值重复的元素，使表中所有元素的值均不同。
     if(L.length == 0)
         return ;
-    
+    int left = 0, right = 1;
+    while(right < L.length)
+    {
+        if(L.data[right] != L.data[left])
+        {
+            L.data[++left] = L.data[right];
+        }
+        right++;
+    }
 }
 
-void T6(SqList &L)
+SqList* T6(SqList &L1, SqList &L2)
 {
     //将两个有序顺序表合并为一个新的有序顺序表，并由函数返回结果顺序表
+    SqList *new_L;
+    return new_L;
 }
 
 void T7(SqList &L)
@@ -99,9 +109,16 @@ void T7(SqList &L)
     
 }
 
+void T8(SqList &L)
+{
+    //线性表(a1,a2,a3,...,an)中的元素递增有序且按顺序存储于计算机内，要求设计一个算法
+    //完成用最少时间在表中查找数值为x的元素，若找到，则将其与后继元素位置相交换
+    //若找不到，则将其插入表中并使表中元素仍然递增有序
+}
+
 int main()
 {
-    cout << 1283 << endl;
-    cout << 123;
+    cout << "测试输出1" << endl;
+    cout << "测试输出2";
     return 0;
 }
